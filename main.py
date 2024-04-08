445c8e3429b32ecc2d4553502905a48ad116ef32
polbb <paulbessone@gmail.com>
2024-04-08 17:35:57 +0100

    st metrics + container

--- main.py
+++ main.py
@@ -266,15 +266,17 @@ with st.container(border=True):
 
         col1, col2 = st.columns(2)
 
-        with col1:
+        with col1.container(border=True):
             st.metric(label="Percentage of scenarios above 0m", value=f"{percentage_above_0*100:.2f}%", delta=delta_percentage_above_0)
             st.metric(label="Percentage of scenarios above 1m", value=f"{percentage_above_1m*100:.2f}%", delta=delta_percentage_above_1m)
             st.metric(label="Percentage of scenarios above 10m", value=f"{percentage_above_10m*100:.2f}%", delta=delta_percentage_above_10m)
         
-        with col2:
+        with col2.container(border=True):
             st.metric(label="Average", value=f"{average_performance}", delta=delta_average_performance)
             st.metric(label="Max", value=f"{max_performance}", delta=delta_max_performance)
             st.metric(label="Min", value=f"{min_performance}", delta=delta_min_performance)
+
+        div()
         w(df)
         
         # Clearing messages after completion
