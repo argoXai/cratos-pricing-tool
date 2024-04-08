970e2a037b666dbac66cdbe5addbb46f8b3a9303
polbb <paulbessone@gmail.com>
2024-04-08 17:08:11 +0100

    st metrics + container

--- main.py
+++ main.py
@@ -256,12 +256,23 @@ with st.container(border=True):
         max_performance = round(max(performance_stats))
         min_performance = round(min(performance_stats))
 
-        st.metric(label="Percentage of scenarios above 0", value=f"{percentage_above_0*100:.2f}%")
-        st.metric(label="Percentage of scenarios above 1m", value=f"{percentage_above_1m*100:.2f}%")
-        st.metric(label="Percentage of scenarios above 10m", value=f"{percentage_above_10m*100:.2f}%")
-        st.metric(label="Average", value=f"{average_performance}")
-        st.metric(label="Max", value=f"{max_performance}")
-        st.metric(label="Min", value=f"{min_performance}")
+        with st.container(border=True):
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.metric(label="Percentage of scenarios above 0m", value=f"{percentage_above_0*100:.2f}%")
+            with col2:
+                st.metric(label="Percentage of scenarios above 1m", value=f"{percentage_above_1m*100:.2f}%")
+            with col3:
+                st.metric(label="Percentage of scenarios above 10m", value=f"{percentage_above_10m*100:.2f}%")
+        
+        with st.container(border=True):
+            col1, col2, col3 = st.columns(3)
+            with col1:
+                st.metric(label="Average", value=f"{average_performance}")
+            with col2:
+                st.metric(label="Max", value=f"{max_performance}")
+            with col3:
+                st.metric(label="Min", value=f"{min_performance}")
 
         w(df)
         
