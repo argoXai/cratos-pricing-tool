e0a2766e981b20f20aad2c8aa0130867f5a5f4e4
polbb <paulbessone@gmail.com>
2024-06-10 14:05:39 +0100

    Commit v4 RoL

--- main.py
+++ main.py
@@ -351,11 +351,12 @@ with tab2:
     col1, col2, _, _, _, _, _, _ = st.columns([3,3,1,1,1,1,1,1])
     with col1.container(border=True):
         total_n = mapping_dict[option]
+        st.title("CR Adjusted Rate")
         if 'average_rate_on_line' in locals():
             cr_adjusted_rate = average_rate_on_line + (average_rate_on_line * total_n)
-            st.write("CR Adjusted Rate:", cr_adjusted_rate)
+            st.metric(label="CR Adjusted Rate", value=cr_adjusted_rate)
         else:
-            st.write("CR Adjusted Rate: Please run the Portfolio Creator")
+            st.metric(label="CR Adjusted Rate", value="Please run the Portfolio Creator")
 
 with tab3:
     st.title("Business Risk Section")
