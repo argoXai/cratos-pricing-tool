ce4208abcff365c252c4d9167d6d98f23981ab49
polbb <paulbessone@gmail.com>
2024-06-11 09:46:15 +0100

    biz risk

--- main.py
+++ main.py
@@ -395,7 +395,8 @@ with tab4:
 
             col3, _ = st.columns(2)
             with col3.container(border=True):
-
+                st.title('Business Risk')
+                
                 if selected_child:
                     st.metric(label="Sector", value=industry_mapping[selected_child][0])
                     st.metric(label="Within Binder Authority", value=industry_mapping[selected_child][1])
